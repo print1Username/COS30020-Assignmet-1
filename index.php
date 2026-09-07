cac4e44b5a628d32e3c7c786b60fbd9948663590
print1Username <128358949+print1Username@users.noreply.github.com>
2026-09-07 17:57:16 +0800

    add index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>COS30020 Assignment 1</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <h2>Introduction</h2>
+        <p>awdwa</p>
+        <?php
+            echo 'My first PHP script!';
+        ?>
+    </body>
+</html
